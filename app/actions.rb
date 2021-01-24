dc8ec64f802a1d04370da30d5695e0cba86c1f7a
songsongha <songsongha@gmail.com>
2021-01-24 03:15:17 +0000

    fixed Scraper initialization issue

--- app/actions.rb
+++ app/actions.rb
@@ -54,67 +54,65 @@ post '/scrape' do
     password = params[:password]
     admin = Admin.find_by_id(1)
 
-if admin.authenticate(password)
-    lower_range = params[:lower_range]
-    upper_range = params[:upper_range]
+    if admin.authenticate(password)
+        lower_range = params[:lower_range]
+        upper_range = params[:upper_range]
     
-
-    require 'thread/pool'
-    pool = Thread.pool(15)
-
-    database = []
-    (lower_range..upper_range).each do |n|
-        url =  "https://www.jobbank.gc.ca/marketreport/summary-occupation/"
-        url.concat("#{n}", "/ca")
-        entry = Job.find_by(url: url)
-        if entry.present?
-            entry.destroy
-        end
-
-        pool.process do
-            scraper = Scraper.new(url)
-            job_title =  scraper.search(".heading-info").strip
-
-            if job_title.present? 
-                wage_string = scraper.search("#j_id_3d_2_27 .section-value").strip
-                wage = wage_string.tr("$,/a-z ","").to_f
-                wage_unit = wage_string.tr("$.,/0-9 ","")
-
-                if wage_unit == "hour"
-                    wage_hour = wage
-                    wage_year = wage * 2000
-                elsif wage_unit == "year"
-                    wage_year = wage
-                    wage_hour = wage / 2000
-                else
-                    wage_year = 0
-                    wage_hour = 0
-                end
-
-                education = scraper.search("#j_id_3d_2_20 .section-value").strip
-
-                postings = scraper.search("#j_id_3d_2_3z .section-value").strip
-                num_posts = postings.tr("a-z ","").to_i
-
-                job = Job.new({ 
-                job_title: job_title,
-                url: url,
-                wage_hour: wage_hour,
-                wage_year: wage_year,
-                education: education,
-                num_posts: num_posts })
-
-                database << job
+        require 'thread/pool'
+        require './lib/scraper'
+        pool = Thread.pool(15)    
+        database = []
+        (lower_range..upper_range).each do |n|
+            url =  "https://www.jobbank.gc.ca/marketreport/summary-occupation/"
+            url.concat("#{n}", "/ca")
+            entry = Job.find_by(url: url)
+            if entry.present?
+                entry.destroy
             end
-        end
-    end 
-    pool.shutdown 
-    Job.import database
-    @message = "Scraping complete"
-else
-    @message = "Incorrect Password"
-end
-erb(:scrape)    
+
+            pool.process do
+                scraper = Scraper.new(url)
+                job_title =  scraper.search(".heading-info").strip
+                if job_title.present? 
+                    wage_string = scraper.search("#j_id_3d_2_27 .section-value").strip
+                    wage = wage_string.tr("$,/a-z ","").to_f
+                    wage_unit = wage_string.tr("$.,/0-9 ","")
+
+                    if wage_unit == "hour"
+                        wage_hour = wage
+                        wage_year = wage * 2000
+                    elsif wage_unit == "year"
+                        wage_year = wage
+                        wage_hour = wage / 2000
+                    else
+                        wage_year = 0
+                        wage_hour = 0
+                    end
+
+                    education = scraper.search("#j_id_3d_2_20 .section-value").strip
+
+                    postings = scraper.search("#j_id_3d_2_3z .section-value").strip
+                    num_posts = postings.tr("a-z ","").to_i
+
+                    job = Job.new({ 
+                    job_title: job_title,
+                    url: url,
+                    wage_hour: wage_hour,
+                    wage_year: wage_year,
+                    education: education,
+                    num_posts: num_posts })
+
+                    database << job
+                end
+            end
+        end 
+        pool.shutdown 
+        Job.import database
+        @message = "Scraping complete"
+    else
+        @message = "Incorrect Password"
+    end
+    erb(:scrape)    
 
 end
 
